8128d98c43cac0e91cfba0006031c01d290179f4
plsy1 <chisan1230@protonmail.com>
2024-05-16 02:48:49 +0800

    .

--- download_all.sh
+++ download_all.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-curl -O https://raw.githubusercontent.com/niumoo/bing-wallpaper/main/bing-wallpaper.md
+
 
 mkdir -p bing_wallpapers
 
 find bing_wallpapers -type f -size 0 -delete
 
-while IFS='|' read -r date url; do
-    date=$(echo "$date" | tr -d '[:space:]')
-    url=$(echo "$url" | sed -n 's/.*(\(.*\)).*/\1/p')
-
+while read -r line; do
+    date=$(echo "$line" | cut -d '|' -f 1 | tr -d ' ')
     filename="${date//[^0-9-]}.jpg"
     filepath="bing_wallpapers/$filename"
-
     if [ -f "$filepath" ]; then
         echo "Skipping $filename as it already exists."
     else
+        url=$(echo "$line" | grep -oP '(?<=\()([^)]+)(?=\))' | tail -n1)
         wget -O "$filepath" "$url" &
     fi
-
 done < bing-wallpaper.md
 
 wait
