0daf8de672a81cef0102e77ed835ff4b2f0f7e6f
plsy1 <chisan1230@protonmail.com>
2024-05-16 03:23:53 +0800

    .

--- set_today.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-current_directory=$(pwd)
-current_date=$(date +"%Y-%m-%d")
-path="$current_directory/bing_wallpapers/$current_date.jpg"
-
-echo $path
-
-osascript -e "tell application \"System Events\" to set picture of (reference to every desktop) to \"$path\""
---
+++ set_today_us.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+json_data=$(curl -s "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US")
+
+json_url=$(echo "$json_data" | jq -r '.images[0].url')
+
+base_url='https://cn.bing.com'
+echo "JSON URL: $json_url"
+
+new_url="${json_url/1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp/UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4}"
+
+full_url="$base_url$new_url"
+echo "Modified JSON URL: $full_url"
+
+
+save_path="today-us.jpg"
+
+wget -O "$save_path" "$full_url"
+
+current_directory=$(pwd)
+path="$current_directory/$save_path"
+
+echo $path
+
+osascript -e "tell application \"System Events\" to set picture of (reference to every desktop) to \"$path\""
